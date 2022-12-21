75349782b903f7ad8b0012559028f1700af14ed6
Jomar Tigcal <jomar@tigcal.com>
2022-12-20 21:18:17 -0800

    Adding base project for Chapter 16

---
+++ Chapter16/Tip Calculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tipcalculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val amountText: EditText = findViewById(R.id.amount_text)
+        val percentText: EditText = findViewById(R.id.percent_text)
+        val computeButton: Button = findViewById(R.id.compute_button)
+        computeButton.setOnClickListener {
+            val amount = amountText.text.toString().ifBlank { "0" }
+            val percent = percentText.text.toString().ifBlank { "0" }
+            val intent = Intent(this, OutputActivity::class.java).apply {
+                putExtra("amount", amount)
+                putExtra("percent", percent)
+            }
+            startActivity(intent)
+        }
+    }
+
+}
