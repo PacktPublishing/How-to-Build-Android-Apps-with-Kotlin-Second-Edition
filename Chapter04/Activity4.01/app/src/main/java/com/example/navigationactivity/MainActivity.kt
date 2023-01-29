77e8c4a7ca5a8dd408cdf9847ce07c32c03b140d
Alex Forrester <aforresterwork@gmail.com>
2023-01-29 15:38:43 +0000

    Chapter 4 Activity update
    * Updates Chapter 4 Activity 1
    
    * Updates Activity solutions

---
+++ Chapter04/Activity4.01/app/src/main/java/com/example/navigationactivity/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.navigationactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment import androidx.navigation.ui.AppBarConfiguration import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration:
+            AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as
+                NavHostFragment
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(setOf( R.id.nav_home, R.id.nav_account, R.id.nav_profile, R.id.nav_mysports))
+        setupActionBarWithNavController(navController,
+            appBarConfiguration)
+        findViewById<BottomNavigationView>(R.id.nav_view)
+            ?.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController
+                = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
