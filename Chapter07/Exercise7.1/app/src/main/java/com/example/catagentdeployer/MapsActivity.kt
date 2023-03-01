1fdc19d04464e41723f1dcf816eb2b765b5666be
Eran Boudjnah <eranbou@gmail.com>
2023-03-01 10:49:27 +0000

    [Chapter07] Updated the permissions implementation.

--- Chapter07/Exercise7.1/app/src/main/java/com/example/catagentdeployer/MapsActivity.kt
+++ Chapter07/Exercise7.1/app/src/main/java/com/example/catagentdeployer/MapsActivity.kt
@@ -1,21 +1,22 @@
 package com.example.catagentdeployer
 
-import android.Manifest
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.appcompat.app.AlertDialog
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
+import androidx.core.content.ContextCompat.checkSelfPermission
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 
-private const val PERMISSION_CODE_REQUEST_LOCATION = 1
-
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +26,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        requestPermissionLauncher =
+            registerForActivityResult(RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    getLastLocation()
+                } else {
+                    showPermissionRationale {
+                        requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+                    }
+                }
+            }
     }
 
     /**
@@ -39,63 +51,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        if (hasLocationPermission()) {
-            getLastLocation()
-        } else {
-            requestPermissionWithRationaleIfNeeded()
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        when (requestCode) {
-            PERMISSION_CODE_REQUEST_LOCATION -> if (
-                grantResults[0] == PackageManager.PERMISSION_GRANTED
-            ) {
-                getLastLocation()
-            } else {
-                requestPermissionWithRationaleIfNeeded()
+        when {
+            hasLocationPermission() -> getLastLocation()
+            shouldShowRequestPermissionRationale(this, ACCESS_FINE_LOCATION) -> {
+                showPermissionRationale {
+                    requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+                }
             }
+            else -> requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
         }
-    }
-
-    private fun requestLocationPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-            PERMISSION_CODE_REQUEST_LOCATION
-        )
     }
 
     private fun hasLocationPermission() =
-        ContextCompat.checkSelfPermission(
-            this, Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-
-    private fun requestPermissionWithRationaleIfNeeded() = if (
-        ActivityCompat.shouldShowRequestPermissionRationale(
-            this, Manifest.permission.ACCESS_FINE_LOCATION
-        )
-    ) {
-        showPermissionRationale {
-            requestLocationPermission()
-        }
-    } else {
-        requestLocationPermission()
-    }
+        checkSelfPermission(this, ACCESS_FINE_LOCATION) ==
+            PERMISSION_GRANTED
 
     private fun showPermissionRationale(positiveAction: () -> Unit) {
         AlertDialog.Builder(this)
             .setTitle("Location permission")
             .setMessage("This app will not work without knowing your current location")
             .setPositiveButton(
-                "OK"
+                android.R.string.ok
             ) { _, _ -> positiveAction() }
+            .setNegativeButton(
+                android.R.string.cancel
+            ) { dialog, _ -> dialog.dismiss() }
             .create()
             .show()
     }
